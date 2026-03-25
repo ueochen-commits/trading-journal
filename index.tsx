65fcafaf9f4857a83039159665640fa80a5c01a1
Zhixun Chen <zhixunchen@outlook.com>
2026-03-25 09:08:16 +0800

    feat: 集成 Sentry 错误追踪

--- index.tsx
+++ index.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import posthog from 'posthog-js';
+import * as Sentry from '@sentry/react';
 
 posthog.init('phc_Bue0oaCzWqHfduKWkCejiDFljJIU0ehvImdGVGMwZdr0', {
   api_host: 'https://us.i.posthog.com',
   person_profiles: 'identified_only',
+});
+
+Sentry.init({
+  dsn: 'https://19b0bc8a05c3daa283a76a9709356f5f@o4511102439260160.ingest.us.sentry.io/4511102451187712',
+  integrations: [Sentry.browserTracingIntegration()],
+  tracesSampleRate: 0.2,
 });
 
 const rootElement = document.getElementById('root');
